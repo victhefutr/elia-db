fc17d7e91333f0d3b5a1b512bb9c08af2dd75ae1
Chukwuemeka Nweze <chukwuemekanweze@Chukwuemekas-MacBook-Air.local>
2025-04-16 04:15:30 +0100

    add code for reading and writing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,32 @@
-fn main() {
-    println!("Hello, world!");
+use std::fs::File;
+use std::fs::OpenOptions;
+use std::io::{self, Write};
+
+fn write_to_file(data: &str)  -> io::Result<()> {
+    let mut file = File::create("output.txt")?;
+    file.write_all(data.as_bytes())?;
+    file.flush()?;
+    file.sync_all()?;
+    Ok(())
+
+}
+
+fn write_to_existing_file(data: &str) -> io::Result<()> {
+  let mut file = OpenOptions::new()
+  .write(true)
+  .append(true)
+  .create(true)
+  .open("output.txt")?;
+
+  file.write_all(data.as_bytes())?;
+  file.flush()?;
+  file.sync_all()?;
+  Ok(())
+}
+
+fn main()  -> io::Result<()> {
+    let data : &str = "Hello, world!";
+    write_to_existing_file(data)?;
+    println!("Data written to file successfully.");
+    Ok(())
 }
